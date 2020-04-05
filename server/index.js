436b3b99d6d78fe0e5e797c431efcbeda171aadd
Franco Arratia <franck_ral@hotmail.com>
2020-04-04 22:23:04 -0400

    fixing a deployment issue in heroku

--- server/index.js
+++ server/index.js
@@ -1,34 +1,38 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys');
+//const mongoose = require('mongoose');
+//const cookieSession = require('cookie-session');
+//const passport = require('passport');
+//const keys = require('./config/keys');
 
-require('./models/User');
-require('./services/passport');
+//require('./models/User');
+//require('./services/passport');
 // const authRoutes = require('./routes/authRoutes'); // Approach 1
 
 
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Emaily service has connected to MongoDB Cluster successfuly...');
-    })
-    .catch(error => {
-        console.log('Something bad ocurred: ', error);
-    });
+// mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => {
+//         console.log('Emaily service has connected to MongoDB Cluster successfuly...');
+//     })
+//     .catch(error => {
+//         console.log('Something bad ocurred: ', error);
+//     });
 
 const app = express();
-app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(
+//     cookieSession({
+//         maxAge: 30 * 24 * 60 * 60 * 1000,
+//         keys: [keys.cookieKey]
+//     })
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // authRoutes(app); // Approach 1
-require('./routes/authRoutes')(app); // Approach 2
+// require('./routes/authRoutes')(app); // Approach 2
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'How are you doing there!' });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
