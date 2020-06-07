a80fcd690f25a276fe4c202f8e4b9ae314dde5d2
Franco Arratia <franck_ral@hotmail.com>
2020-06-07 19:01:31 -0400

    reactjs-client-development: Section7 - Basics of redux thunk

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,5 +2,15 @@ import axios from 'axios';
 import { FETCH_USER } from "./types";
 
 const fetchUser = () => {
-  axios.get('/api/current_user');
+  // const request = axios.get('/api/current_user');
+
+  // return {
+  //   type: FETCH_USER,
+  //   payload: request
+  // };
+
+  return function(dispatch) {
+    axios.get('/api/current_user')
+      .then(res => dispatch({ type: FETCH_USER, payload: res }));
+  }
 };
