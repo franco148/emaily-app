36b1903cf99a7663e3a13060ea43aa3c4c4d72c4
Franco Arratia <franck_ral@hotmail.com>
2020-06-09 21:56:56 -0400

    reactjs-client-development: Refactoring actions using Async & Await

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,16 +1,25 @@
 import axios from 'axios';
 import { FETCH_USER } from "./types";
 
-export const fetchUser = () => {
-  // const request = axios.get('/api/current_user');
+// FIRST VERSION: Taking into account promises.
+// export const fetchUser = () => {
+//   // const request = axios.get('/api/current_user');
 
-  // return {
-  //   type: FETCH_USER,
-  //   payload: request
-  // };
+//   // return {
+//   //   type: FETCH_USER,
+//   //   payload: request
+//   // };
 
-  return function(dispatch) {
-    axios.get('/api/current_user')
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  }
-};
+//   return function(dispatch) {
+//     axios.get('/api/current_user')
+//       .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//   }
+// };
+
+// SECOND VERSION: Async & Await
+export const fetchUser = () =>
+  async dispatch => {
+    const currentUserRes = await axios.get('/api/current_user');
+    dispatch({ type: FETCH_USER, payload: currentUserRes });
+  };
+
