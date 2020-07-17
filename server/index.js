176ebe82d10f9065a3059a2520d4b7b147dda37a
Franco Arratia <franck_ral@hotmail.com>
2020-07-17 19:05:54 -0400

    reactjs-handling-payments: Section 8 - BodyParser Middleware

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
 require('./models/User');
@@ -18,6 +19,7 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: tru
     });
 
 const app = express();
+app.use(bodyParser.json());
 app.use(
     cookieSession({
       maxAge: 30 * 24 * 60 * 60 * 1000,
