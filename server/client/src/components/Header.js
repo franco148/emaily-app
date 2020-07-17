a9a112c7f195023baec47cec36a5f0be6c070abc
Franco Arratia <franck_ral@hotmail.com>
2020-07-17 17:54:18 -0400

    reactjs-handling-payments: Section 8 - Stripe Tokens

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
+import Payments from "./Payments";
+
 class Header extends Component {
 
   renderContent() {
@@ -16,7 +18,10 @@ class Header extends Component {
         );
       default:
         // return 'I am logged in.';
-        return <li><a href="/api/logout">Logout</a></li>
+        return [
+          <li><Payments></Payments></li>,
+          <li><a href="/api/logout">Logout</a></li>
+        ];
     }
   }
 
