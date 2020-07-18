64362a6c46d43756093c127b7f4eceba643ef175
Franco Arratia <franck_ral@hotmail.com>
2020-07-18 17:42:13 -0400

    reactjs-handling-payments: Section 9 - Route-Specific Middlewares

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,8 +1,16 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  app.post('/api/stripe', async (req, res) => {
+  app.post('/api/stripe', requireLogin,async (req, res) => {
+    /**
+     * * This piece of code is replaced by the middleware created (required login)
+     */
+    // if (!req.user) {
+    //   return res.status(401).send({ error: 'You must log in!' });
+    // }
+
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
