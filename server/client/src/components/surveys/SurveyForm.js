3fac1cee39fa7eedf6bab3321942dd467606c1b6
Franco Arratia <franck_ral@hotmail.com>
2020-07-24 13:12:47 -0400

    reactjs-frontend-surveys: Section 11 - Validating Emails & Displaying Invalid Emails

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -5,6 +5,7 @@ import { reduxForm, Field } from "redux-form";
 import { Link } from 'react-router-dom';
 
 import SurveyField from "./SurveyField";
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
   { label: 'Survey Title', name: 'title' },
@@ -67,6 +68,8 @@ function validate(values) {
   // if (!values.ebody) {
   //   errors.title = 'You must provide a body';
   // }
+
+  errors.emails = validateEmails(values.emails || '');
 
   _.each(FIELDS, ({ name }) => {
     if (!values[name]) {
