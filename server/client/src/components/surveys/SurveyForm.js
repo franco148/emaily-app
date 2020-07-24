436cc3a6978c6338f476be87614d1dd10c45b465
Franco Arratia <franck_ral@hotmail.com>
2020-07-24 11:22:24 -0400

    reactjs-frontend-surveys: Section 11 - Fields from Config

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,19 +1,36 @@
+import _ from 'lodash';
+
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 
 import SurveyField from "./SurveyField";
 
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'ebody' },
+  { label: 'Recipient List', name: 'emails' }
+];
+
 class SurveyForm extends Component {
 
   renderFields() {
-    return (
-      <div>
-        <Field label="Survey Title" type="text" name="title" component={SurveyField} />
-        <Field label="Subject Line" type="text" name="subject" component={SurveyField} />
-        <Field label="Email Body" type="text" name="ebody" component={SurveyField} />
-        <Field label="Recipient List" type="text" name="emails" component={SurveyField} />
-      </div>
-    );
+    // return (
+    //   <div>
+    //     <Field label="Survey Title" type="text" name="title" component={SurveyField} />
+    //     <Field label="Subject Line" type="text" name="subject" component={SurveyField} />
+    //     <Field label="Email Body" type="text" name="ebody" component={SurveyField} />
+    //     <Field label="Recipient List" type="text" name="emails" component={SurveyField} />
+    //   </div>
+    // );
+
+    // return _.map(FIELDS, field => {
+    //   return <Field component={SurveyField} type="text" label={field.label} />
+    // });
+
+    return _.map(FIELDS, ({ label, name }) => {
+      return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+    });
   }
 
   render() {
