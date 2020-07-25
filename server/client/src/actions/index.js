4c1faf43e97bab6cc679e6a667350e0fb596fa2e
Franco Arratia <franck_ral@hotmail.com>
2020-07-25 19:15:35 -0400

    reactjs-home-stretch: Section 13 - Wiring Surveys Up to Redux & Wiring React to Redux

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from "./types";
+import { FETCH_USER, FETCH_SURVEYS } from "./types";
 
 // FIRST VERSION: Taking into account promises.
 // export const fetchUser = () => {
@@ -33,4 +33,10 @@ export const submitSurvey = (values, history) => async dispatch => {
 
   history.push('/surveys');
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+
+  dispatch({ type: FETCH_SURVEYS, payload: res.data })
 };
