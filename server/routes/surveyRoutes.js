cbb8f2f8db2d3b8577f0b0f895e5c9d31f34b387
Franco Arratia <franck_ral@hotmail.com>
2020-07-25 18:49:14 -0400

    reactjs-home-stretch: Section 13 - Whitelisting Model Fields

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -100,4 +100,10 @@ module.exports = app => {
   app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
   });
+
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id });
+
+    res.send(surveys);
+  });
 };
