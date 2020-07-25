ed903193d648f6f215c7b4e93c43f1083962e256
Franco Arratia <franck_ral@hotmail.com>
2020-07-25 19:26:44 -0400

    reactjs-home-stretch: Section 13 - Rendering a list of surveys

--- server/client/src/components/surveys/SurveyList.js
+++ server/client/src/components/surveys/SurveyList.js
@@ -8,10 +8,28 @@ class SurveyList extends Component {
     this.props.fetchSurveys();
   }
 
+  renderSurveys() {
+    return this.props.surveys.map(({ _id, title, body, dateSent, yes, no}) => {
+      return (
+        <div className="card darken-1" key={_id}>
+          <div className="card-content">
+            <span className="card-title">{title}</span>
+            <p>{body}</p>
+            <p className="right">Sent ON: {new Date(dateSent).toLocaleDateString()}</p>
+          </div>
+          <div className="card-action">
+            <a>Yes: { yes }</a>
+            <a>No: { no }</a>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        SurveyList
+        { this.renderSurveys()}
       </div>
     );
   }
